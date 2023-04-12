3647e86d00e2f70c6bd7ba5713183a6ea7b44e5b
Vivek <viveknimbalkar48@gmail.com>
2023-04-12 11:35:28 +0530

    completed activites XML

---
+++ app/src/main/java/com/example/pothole_detection_system/detection_model.java
@@ -0,0 +1,14 @@
+package com.example.pothole_detection_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detection_model extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detection_model);
+    }
+}
